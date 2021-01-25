4ef22b1b6221e8c85eb43cea04d75fe307ed8d64
couldbefree <zoriaalexandr@gmail.com>
2021-01-25 21:16:47 +0200

    made button

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,17 @@
-/*
 $(document).ready(function() {
-    $('.slider').slick({
+    /*$('.slider').slick({
         dots: true
+    });*/
+    const button = $('#button');
+    const intro = $('#intro');
+    const introSectionHeight = intro.height();
+    $(window).scroll(function () {
+        const scroll = $(window).scrollTop();
+        if(scroll > introSectionHeight) {
+            button.removeClass('hidden');
+            //animate__bounceOut
+        } else {
+            button.addClass('hidden');
+        }
     });
-})*/
+})
