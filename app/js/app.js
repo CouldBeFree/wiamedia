f64422349f4edf41f457d8943432f91fa42e5227
couldbefree <zoriaalexandr@gmail.com>
2021-01-31 09:27:42 +0200

    made responsive design

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,4 @@ $(document).ready(function() {
     /*$('.slider').slick({
         dots: true
     });*/
-    const button = $('#button');
-    const intro = $('#intro');
-    const introSectionHeight = intro.height();
-    $(window).scroll(function () {
-        const scroll = $(window).scrollTop();
-        if(scroll > introSectionHeight) {
-            button.removeClass('hidden');
-        } else {
-            button.addClass('hidden');
-        }
-    });
 })
