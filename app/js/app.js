1ddd644b8ca8cd6f33ace28f5c8822daddd6aa65
couldbefree <zoriaalexandr@gmail.com>
2021-02-14 08:26:25 +0200

    finished checkout

--- app/js/app.js
+++ app/js/app.js
@@ -88,4 +88,34 @@ $(document).ready(function() {
         return !(charCode > 31 && (charCode < 48 || charCode > 57));
     }
     cardInput.on('keypress', isNumber);
+
+    const cvc = $('#cvc');
+    cvc.on('keypress', isNumber);
+
+    var date = document.getElementById('date');
+
+    function checkValue(str, max) {
+        if (str.charAt(0) !== '0' || str == '00') {
+            var num = parseInt(str);
+            if (isNaN(num) || num <= 0 || num > max) num = 1;
+            str = num > parseInt(max.toString().charAt(0))
+            && num.toString().length == 1 ? '0' + num : num.toString();
+        }
+        return str;
+    }
+
+    date.addEventListener('input', function(e) {
+        this.type = 'text';
+        var input = this.value;
+        if (/\D\/$/.test(input)) input = input.substr(0, input.length - 1);
+        var values = input.split('/').map(function(v) {
+            return v.replace(/\D/g, '')
+        });
+        if (values[0]) values[0] = checkValue(values[0], 12);
+        if (values[1]) values[1] = checkValue(values[1], 31);
+        var output = values.map(function(v, i) {
+            return v.length == 2 && i < 2 ? v + '/' : v;
+        });
+        this.value = output.join('').substr(0, 5);
+    });
 })
